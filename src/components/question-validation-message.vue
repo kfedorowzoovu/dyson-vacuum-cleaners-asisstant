de07134a5bcfa9eab7b1419d408ac9ed5ba32429
Grzegorz Laszczyk <g.laszczyk@zoovu.com>
2021-08-19 12:01:14 +0200

    Add alert to validation message

---
+++ src/components/question-validation-message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="question.hasNotValidAnswer" :class="componentStyle.container">
+    <p
+      v-dompurify-html="$t(question.validationErrorCodes[0])"
+      class="question-validation-message"
+      aria-hidden="true"
+    ></p>
+    <span role="alert" aria-live="assertive" class="hidden-description">{{
+      $t(question.validationErrorCodes[0])
+    }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from "@zoovu/runner-browser-api";
+import { QuestionValidationMessageView as QuestionValidationMessageViewBase } from "@zoovu/runner-web-design-base";
+
+@Component
+export default class QuestionValidationMessageView extends QuestionValidationMessageViewBase {}
+</script>
